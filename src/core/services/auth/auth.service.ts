37a194256494ae507d73c94a071b62260580bc15
muhammadhisam pala <sammywin090@gmail.com>
2024-05-31 17:38:06 +0700

    fix: add auth service

--- src/core/services/auth/auth.service.ts
+++ src/core/services/auth/auth.service.ts
@@ -1,12 +1,12 @@
-import { Effect } from "effect";
-import { AuthInputType } from "../../../types";
-import * as service_users from "../users";
-import * as error from "./error/";
-const auth = (item: AuthInputType) => {};
-const valid_username = (username: string) => {
-  return Effect.tryPromise({
-    try: async () => await service_users.get_item_by_username(username),
-    catch: (err: unknown) => new error.valid_username_error((err as Error).message)
-  });
-};
-export {};
+import { Effect, pipe } from "effect"
+import { AuthInputType } from "../../../types"
+import * as service_users from "../users"
+import * as error from "./error/"
+const auth = (item:AuthInputType)=> pipe(valid_username(item.username))
+const valid_username = (username:string)=>{
+    return Effect.tryPromise({
+        try:async()=> await service_users.get_item_by_username(username),
+        catch:(err:unknown)=> new error.valid_username_error((err as Error).message)
+    })
+}
+export {auth}
